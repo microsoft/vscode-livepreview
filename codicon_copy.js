ba2489314cc95ef7b353ec34c2d8a8962654c291
Andrea Mah <andrea_mah22@hotmail.com>
2021-06-04 09:51:34 -0600

    add codicon ocpy

---
+++ codicon_copy.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+let outputRoot = __dirname;
+const outDir = path.join(outputRoot, 'media');
+
+fs.copyFileSync (
+	path.join(__dirname, 'node_modules/vscode-codicons/dist/codicon.css'),
+	path.join(outDir, 'codicon.css'));
+
+fs.copyFileSync (
+	path.join(__dirname, 'node_modules/vscode-codicons/dist/codicon.ttf'),
+	path.join(outDir, 'codicon.ttf'));
