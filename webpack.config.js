e2b6a58b7af6916e8ba2f2601a65446ab4127916
Tyler Leonhardt <me@tylerleonhardt.com>
2022-05-17 18:00:08 -0700

    onboard to loc

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,15 @@ const config = {
 	module: {
 		rules: [
 			{
+				// vscode-nls-dev loader:
+				// * rewrite nls-calls
+				loader: 'vscode-nls-dev/lib/webpack-loader',
+				options: {
+					// start with this being set to where your package.json is
+					base: __dirname,
+				},
+			},
+			{
 				test: /\.ts$/,
 				exclude: /node_modules/,
 				use: 'ts-loader',
