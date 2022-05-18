fe2458fb64c837747a9e70db09a8b86a496f9427
Tyler James Leonhardt <me@tylerleonhardt.com>
2022-05-17 22:06:40 -0700

    more calls

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,7 @@
 import * as vscode from 'vscode';
+import * as nls from 'vscode-nls';
+
+const localize = nls.loadMessageBundle();
 
 export const WS_URL_PLACEHOLDER = '${WS_URL}';
 export const HTTP_URL_PLACEHOLDER = '${HTTP_URL}';
@@ -6,18 +9,18 @@ export const HTTP_URL_PLACEHOLDER = '${HTTP_URL}';
 export const INIT_PANEL_TITLE = '/';
 
 export const DONT_SHOW_AGAIN: vscode.MessageItem = {
-	title: "Don't Show Again",
+	title: localize('dont show again', "Don't Show Again"),
 };
 
 export const OPEN_EXTERNALLY: vscode.MessageItem = {
-	title: 'Open Externally',
+	title: localize('open externally', 'Open Externally'),
 };
 
 export const DEFAULT_HOST = '127.0.0.1';
 
 export const EXTENSION_ID = 'ms-vscode.live-server';
 
-export const OUTPUT_CHANNEL_NAME = 'Embedded Live Preview Console';
+export const OUTPUT_CHANNEL_NAME = localize('output channel name', 'Embedded Live Preview Console');
 
 export const INJECTED_ENDPOINT_NAME = '/___vscode_livepreview_injected_script';
 
@@ -30,4 +33,4 @@ export const UriSchemes: any = {
 
 export const LIVE_PREVIEW_SERVER_ON = 'LivePreviewServerOn';
 
-export const TASK_TERMINAL_BASE_NAME = 'Run Server';
+export const TASK_TERMINAL_BASE_NAME = localize('task name', 'Run Server');
