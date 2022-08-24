16834d68c80d437e3b32bd9b46765ce9aaea245c
Tyler James Leonhardt <me@tylerleonhardt.com>
2022-08-24 17:52:43 +0000

    include package step and validateStable

---
+++ scripts/validateStable.js
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+'use strict';
+const fs = require('fs');
+
+const json = JSON.parse(fs.readFileSync('./package.json').toString());
+const stableVersion = json.version.match(/(\d+)\.(\d+)\.(\d+)/);
+const minor = stableVersion[2];
+
+if (Number.parseInt(minor) % 2 !== 0) {
+	// Pre-release documentation recommends that stable releases follow major.EVEN_NUMBER.patch
+	// and pre-releases follow major.ODD_NUMBER.patch. See
+	// https://code.visualstudio.com/api/working-with-extensions/publishing-extension#prerelease-extensions
+	throw new Error('Stable extension version number must always be even.');
+}
