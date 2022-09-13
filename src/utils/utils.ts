a3f7b7a9020d708cef16ae939e9d6c2697196faf
andreamah <andrea_mah22@hotmail.com>
2022-09-13 16:13:01 -0700

    merge and resolve conflicts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -60,17 +60,3 @@ function hasInjectableFileEnding(file: string): boolean {
 	const supportedEndings = ['.html', '.htm', '.xhtml'];
 	return supportedEndings.find((ending) => file.endsWith(ending)) !== undefined;
 }
-
-/**
- * @description Generates the randomized nonce for the webview (embedded preview).
- * @returns 64-long randomized nonce.
- */
-export function getNonce(): string {
-	let text = '';
-	const possible =
-		'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	for (let i = 0; i < 64; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
-}
