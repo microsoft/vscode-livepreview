672a5c0f7deabe1cfe0d61e7f7076d97a46ead63
Andrea Mah <31675041+andreamah@users.noreply.github.com>
2022-10-24 15:37:21 -0700

    header/header eslint rule to enforce copyright headers (#369)
    * eslint rules
    
    * Add eslint rules to enfoce copyright headers
    Fixes #349

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 /**@type {import('eslint').Linter.Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
+	plugins: ['@typescript-eslint', 'header'],
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
 	rules: {
 		semi: [2, 'always'],
@@ -12,5 +17,15 @@ module.exports = {
 		'@typescript-eslint/explicit-module-boundary-types': 0,
 		'@typescript-eslint/no-non-null-assertion': 0,
 		'@typescript-eslint/explicit-function-return-type':1,
+		"header/header": [
+			2,
+			"block",
+			[
+				"---------------------------------------------------------------------------------------------",
+				" *  Copyright (c) Microsoft Corporation. All rights reserved.",
+				" *  Licensed under the MIT License. See License.txt in the project root for license information.",
+				" *--------------------------------------------------------------------------------------------"
+			]
+		]
 	},
 };
