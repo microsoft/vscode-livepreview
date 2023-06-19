a1b96b821f2f1121f2c5327eb27376307ad1e457
Andrea Mah <31675041+andreamah@users.noreply.github.com>
2023-06-19 11:39:28 -0700

    Support custom external browser on remote cases (SSH, Dev Container) (#520)
    * Support custom external browser on remote cases (SSH, Dev Container)
    Fixes #517

--- src/utils/externalBrowserUtils.ts
+++ src/utils/externalBrowserUtils.ts
@@ -4,48 +4,20 @@
  *--------------------------------------------------------------------------------------------*/
 import { CustomExternalBrowser } from "./settingsUtil";
 import * as vscode from 'vscode';
-import * as JSDebugBrowsers from '@vscode/js-debug-browsers';
-import open from "open";
 
 export class ExternalBrowserUtils {
 
 	static async openInBrowser(target: string, browser: CustomExternalBrowser): Promise<void> {
-
-		if (vscode.env.appHost !== 'desktop') {
+		if (vscode.env.appHost !== 'desktop' || browser === CustomExternalBrowser.default) {
 			vscode.env.openExternal(vscode.Uri.parse(target));
 			return;
 		}
 
-		if (browser === CustomExternalBrowser.default) {
-			vscode.env.openExternal(vscode.Uri.parse(target));
-			return;
-		}
 		try {
-			let browserFinder: JSDebugBrowsers.IBrowserFinder | undefined;
-			switch (browser) {
-				case CustomExternalBrowser.chrome:
-					browserFinder = new JSDebugBrowsers.ChromeBrowserFinder();
-					break;
-				case CustomExternalBrowser.edge:
-					browserFinder = new JSDebugBrowsers.EdgeBrowserFinder();
-					break;
-				case CustomExternalBrowser.firefox:
-					browserFinder = new JSDebugBrowsers.FirefoxBrowserFinder();
-					break;
-			}
-
-			const exe = await browserFinder?.findWhere(() => true);
-			if (exe) {
-				await open(target, { app: { name: exe.path } });
-			} else {
-				vscode.window.showErrorMessage(`Could not find ${browser} installation. Please make sure it is installed or change the external preview browser in your settings.`,
-					'Open Settings').then((value) => {
-						if (value) {
-							vscode.commands.executeCommand('workbench.action.openSettings', 'livePreview.customExternalBrowser');
-						}
-					});
-			}
+			const browserStr = browser.toLowerCase(); // the debug companion expects lowercase browser names
+			vscode.commands.executeCommand('js-debug-companion.launch', {browserType: browserStr, URL: target});
 		} catch (e) {
+
 			vscode.env.openExternal(vscode.Uri.parse(target));
 		}
 	}
